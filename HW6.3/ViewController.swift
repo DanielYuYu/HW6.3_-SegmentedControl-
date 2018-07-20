3b504537c4c51134a528c996204fc7d5c9366560
Daniel <daniel@macbook-pro.local>
2018-07-20 10:17:07 +0800

    HomeWork6.3 First commit 使用 Segmented Control 做出更換圖片效果

--- HW6.3/ViewController.swift
+++ HW6.3/ViewController.swift
@@ -14,7 +14,23 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    @IBOutlet weak var outletImageView: UIImageView!
+    
+    @IBAction func pressedSegmented(_ sender: UISegmentedControl) {
+    // 如果要取得Segment的標題文字，使用 let name = segmentedControl.titleForSegment(at: 0)
+    // 如果要取得Segment的index則使用sender.selectedSegmentIndex == 0
+        if ( sender.selectedSegmentIndex == 0){
+            outletImageView.image = UIImage(named: "0")
+        }
+        else if ( sender.selectedSegmentIndex == 1){
+            outletImageView.image = UIImage(named: "1")
+        }
+        else{
+            outletImageView.image = UIImage(named: "2")	
+        }
+        
+    }
+    
+   
 }
 
